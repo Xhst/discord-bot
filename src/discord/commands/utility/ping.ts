f1996b54157caf266443dc5ef009893cddb9bce3
Mattia <82611659+Xhst@users.noreply.github.com>
2024-09-15 23:20:00 +0200

    chore: ping command i18n

--- src/discord/commands/utility/ping.ts
+++ src/discord/commands/utility/ping.ts
@@ -1,10 +1,11 @@
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 import { Command } from "@discord/commands";
+import { i18n } from "@i18n/internationalization";
 
 const command: Command = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with Pong!"),
+    .setDescription(i18n("commands.utility.ping.description")),
 
   async execute(interaction: CommandInteraction): Promise<void> {
     await interaction.reply("Pong!");
