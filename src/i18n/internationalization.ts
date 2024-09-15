a0c391cd1563022dfdf771298221a05b78bb3814
Mattia <82611659+Xhst@users.noreply.github.com>
2024-09-15 23:19:13 +0200

    chore: i18n array separator

--- src/i18n/internationalization.ts
+++ src/i18n/internationalization.ts
@@ -1,7 +1,6 @@
 import en from '@assets/i18n/en.json';
-import it from '@assets/i18n/it.json';
 
-type JsonLeaf = string | number | boolean | null | undefined | JsonLeaf[];
+type JsonLeaf = string | number | boolean | null | undefined;
 
 type Join<K, P> = K extends JsonLeaf ?
     P extends JsonLeaf ?
@@ -9,16 +8,17 @@ type Join<K, P> = K extends JsonLeaf ?
     : never : never;
 
 type Prev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]]
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]];
 
 type Paths<T, D extends number = 10> = [D] extends [never] ? never : T extends object ?
     { [K in keyof T]-?: K extends JsonLeaf ?
         `${K}` | Join<K, Paths<T[K], Prev[D]>>
         : never
-    }[keyof T] : ""
+    }[keyof T] : "";
 
-type LanguageKey = Paths<typeof en>
+type LanguageKey = Paths<typeof en>;
 
+export const arraySeparator = ' | ';
 
 function flattenJSON(data: any, parentKey = '', result: { [key: string]: JsonLeaf } = {}): { [K in LanguageKey]: JsonLeaf } {
     for (const key in data) {
@@ -26,6 +26,8 @@ function flattenJSON(data: any, parentKey = '', result: { [key: string]: JsonLea
             const newKey = parentKey ? `${parentKey}.${key}` : key;
             if (typeof data[key] === 'object' && !Array.isArray(data[key])) {
                 flattenJSON(data[key], newKey, result);
+            } else if (Array.isArray(data[key])) {
+                result[newKey] = data[key].join(arraySeparator);
             } else {
                 result[newKey] = data[key];
             }
@@ -36,13 +38,14 @@ function flattenJSON(data: any, parentKey = '', result: { [key: string]: JsonLea
 
 const translations: { [lang: string]: { [K in LanguageKey]: JsonLeaf } } = {
     en: flattenJSON(en),
-    it: flattenJSON(it),
-}
+};
 
 export function i18n(key: LanguageKey, language?: string): string {
     if (!language) language = getLanguage();
 
-    return translations[language][key]?.toString() || translations['en'][key]?.toString() || key;
+    return translations[language.toLocaleLowerCase()][key]?.toString() 
+        || translations['en'][key]?.toString() 
+        || key;
 }
 
 export function getLanguage(): string {
