46d548a3c21a2d3552ca252e10141dd0970c905d
Mattia <82611659+Xhst@users.noreply.github.com>
2024-09-15 23:57:39 +0200

    feat: random activity

--- src/discord/events/clientReady.ts
+++ src/discord/events/clientReady.ts
@@ -1,6 +1,7 @@
-import { Client, Events } from "discord.js";
+import { Client, Events, ActivityType } from "discord.js";
 import { ClientWithCommands } from "../client";
 import { Event } from "@discord/events";
+import { arraySeparator, i18n } from "@src/i18n/internationalization";
 
 const event: Event = {
     name: Events.ClientReady,
@@ -11,7 +12,25 @@ const event: Event = {
         if (client.user === null) return;
 
         console.log(`Ready! Logged in as ${client.user.tag}`);
+
+        setupActivity(client);
     }
 };
+
+function setupActivity(client: ClientWithCommands): void {
+    const randomActivityInterval = 30000;
+    const activities = i18n("activity").split(arraySeparator);
+
+    function randomActivity() {
+        const randomIndex = Math.floor(Math.random() * activities.length);
+
+        const newActivity = activities[randomIndex];
+
+        client.user!.setActivity(newActivity, { type: ActivityType.Custom });
+    }
+
+    randomActivity();
+    setInterval(() => randomActivity(), randomActivityInterval);
+}
 
 export default event;
