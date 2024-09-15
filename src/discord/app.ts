083bce9019e26a5f6eaacd90f1cc3922b2daec17
Mattia <82611659+Xhst@users.noreply.github.com>
2024-09-15 15:15:06 +0200

    feat: register commands

--- src/discord/app.ts
+++ src/discord/app.ts
@@ -1,6 +1,7 @@
 import { Client, Collection, GatewayIntentBits } from "discord.js";
-import { ClientWithCommands } from "./client";
 import { Command, deployCommands } from "./commands";
+import { ClientWithCommands } from "./client";
+import { readDir } from "../utils";
 import { join } from "path";
 
 
@@ -28,8 +29,27 @@ export class DiscordApp {
     }
 
     public async start(): Promise<void> {
+        await this.registerCommands();
         await deployCommands();
 
         await this.client.login(process.env.DISCORD_TOKEN);
     }
+
+    private async registerCommands(): Promise<void> {
+        const commandFiles = readDir(this.commandsPath);
+
+        for (const file of commandFiles) {
+
+            // Dynamically import the command module
+            const command: Command = (await import(file)).default;
+
+            if (!command || !command.data || !command.execute) {
+                console.error(`Cannot register command, invalid command file: ${file}`);
+                continue;
+            }
+
+            this.client.commands.set(command.data.name, command);
+        }
+    }
+    
 }
