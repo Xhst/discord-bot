9ead88a439ed8eb0aa00533a8983296a11b91b6d
Mattia <82611659+Xhst@users.noreply.github.com>
2024-09-15 15:02:38 +0200

    chore: discord start

--- src/discord/app.ts
+++ src/discord/app.ts
@@ -1,10 +1,33 @@
+import { Client, Collection, GatewayIntentBits } from "discord.js";
+import { ClientWithCommands } from "./client";
+import { Command } from "./commands";
+import { join } from "path";
+
+
 export class DiscordApp {
 
+    private client: ClientWithCommands;
+
+    private readonly commandsPath: string;
+    private readonly eventsPath: string;
+
     constructor() {
-       
+        this.client = new Client({
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildVoiceStates,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.MessageContent,
+            ],
+        }) as ClientWithCommands;
+
+        this.commandsPath = join(__dirname, "commands");
+        this.eventsPath = join(__dirname, "events");
+
+        this.client.commands = new Collection<string, Command>();
     }
 
-    public start(): void {
-        
+    public async start(): Promise<void> {
+        await this.client.login(process.env.DISCORD_TOKEN);
     }
 }
