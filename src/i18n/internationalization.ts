fe4e587200bb9365a01cdba7c772c9467f452c59
Mattia <82611659+Xhst@users.noreply.github.com>
2024-09-16 15:40:00 +0200

    style: reordering

--- src/i18n/internationalization.ts
+++ src/i18n/internationalization.ts
@@ -1,40 +1,6 @@
 import en from '@assets/i18n/en.json';
 
-type JsonLeaf = string | number | boolean | null | undefined;
-
-type Join<K, P> = K extends JsonLeaf ?
-    P extends JsonLeaf ?
-    `${K}${"" extends P ? "" : "."}${P}`
-    : never : never;
-
-type Prev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]];
-
-type Paths<T, D extends number = 10> = [D] extends [never] ? never : T extends object ?
-    { [K in keyof T]-?: K extends JsonLeaf ?
-        `${K}` | Join<K, Paths<T[K], Prev[D]>>
-        : never
-    }[keyof T] : "";
-
-type LanguageKey = Paths<typeof en>;
-
 export const arraySeparator = ' | ';
-
-function flattenJSON(data: any, parentKey = '', result: { [key: string]: JsonLeaf } = {}): { [K in LanguageKey]: JsonLeaf } {
-    for (const key in data) {
-        if (data.hasOwnProperty(key)) {
-            const newKey = parentKey ? `${parentKey}.${key}` : key;
-            if (typeof data[key] === 'object' && !Array.isArray(data[key])) {
-                flattenJSON(data[key], newKey, result);
-            } else if (Array.isArray(data[key])) {
-                result[newKey] = data[key].join(arraySeparator);
-            } else {
-                result[newKey] = data[key];
-            }
-        }
-    }
-    return result as { [K in LanguageKey]: JsonLeaf };
-}
 
 const translations: { [lang: string]: { [K in LanguageKey]: JsonLeaf } } = {
     en: flattenJSON(en),
@@ -67,3 +33,39 @@ export function i18n(key: LanguageKey, ...args: Array<string | number>): string 
 export function getLanguage(): string {
     return process.env.LANGUAGE || 'en';
 }
+
+function flattenJSON(data: any, parentKey = '', result: { [key: string]: JsonLeaf } = {}): { [K in LanguageKey]: JsonLeaf } {
+    for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+            const newKey = parentKey ? `${parentKey}.${key}` : key;
+            if (typeof data[key] === 'object' && !Array.isArray(data[key])) {
+                flattenJSON(data[key], newKey, result);
+            } else if (Array.isArray(data[key])) {
+                result[newKey] = data[key].join(arraySeparator);
+            } else {
+                result[newKey] = data[key];
+            }
+        }
+    }
+    return result as { [K in LanguageKey]: JsonLeaf };
+}
+
+// Type definitions
+
+type JsonLeaf = string | number | boolean | null | undefined;
+
+type Join<K, P> = K extends JsonLeaf ?
+    P extends JsonLeaf ?
+    `${K}${"" extends P ? "" : "."}${P}`
+    : never : never;
+
+type Prev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]];
+
+type Paths<T, D extends number = 10> = [D] extends [never] ? never : T extends object ?
+    { [K in keyof T]-?: K extends JsonLeaf ?
+        `${K}` | Join<K, Paths<T[K], Prev[D]>>
+        : never
+    }[keyof T] : "";
+
+type LanguageKey = Paths<typeof en>;
