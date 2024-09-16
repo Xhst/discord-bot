d40d0abf0712f2d83a323b2730f1cdab3e58d277
Mattia <82611659+Xhst@users.noreply.github.com>
2024-09-16 15:56:48 +0200

    docs: discord client with commands

--- src/discord/client.ts
+++ src/discord/client.ts
@@ -1,6 +1,12 @@
 import { Client, Collection } from "discord.js";
 import { Command } from "@discord/commands";
 
+/**
+ * A Discord client with a collection of commands.
+ * 
+ * This interface extends the Discord.js Client class by adding a collection of commands.
+ * It's used to access the commands when handling interactions.
+ */
 export interface ClientWithCommands extends Client {
     commands: Collection<string, Command>;
 }
